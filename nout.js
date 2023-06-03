49908edb1bfa1ea527fe8f5d8e11243838edbebc
fanetattoo <7222538+fanetattoo@users.noreply.github.com>
2023-06-03 15:33:35 +0100

    Update nout.js

--- nout.js
+++ nout.js
@@ -1,13 +1,5 @@
 
-/*!
-   JW Player version 8.27.1
-   Copyright (c) 2023, JW Player, All Rights Reserved
-   This source code and its use and distribution is subject to the terms
-   and conditions of the applicable license agreement.
-   https://www.jwplayer.com/tos/
-   This product includes portions of other software. For the full text of licenses, see
-   https://ssl.p.jwpcdn.com/player/v/8.27.1/notice.txt
-*/(() => {
+(() => {
     var e, t, n = {
             696: (e, t, n) => {
                 "use strict";
